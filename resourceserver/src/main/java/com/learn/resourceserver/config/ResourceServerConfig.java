c98258f03c92b294f746027a4d67581722a85f1f
rushikesh <rushikesh.softvan@gmail.com>
2023-08-07 19:05:56 +0530

    I have completed seperate authorization server and resourse server with inMemory to database

--- resourceserver/src/main/java/com/learn/resourceserver/config/ResourceServerConfig.java
+++ resourceserver/src/main/java/com/learn/resourceserver/config/ResourceServerConfig.java
@@ -1,9 +1,25 @@
 package com.learn.resourceserver.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig {
+public class ResourceServerConfig  {
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(converter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter converter() {
+        var conv = new JwtAccessTokenConverter();
+        conv.setSigningKey("secret"); // symmetric key
+        return conv;
+    }
 }
